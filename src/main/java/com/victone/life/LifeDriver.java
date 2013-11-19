0da76d39b32a2fa227eb8e2ffd4203dc9a64b163
Victor Wilson <victor.wilson@gmail.com>
2013-11-19 00:39:24 -0600

    maven integration. package restructuring.

---
+++ src/main/java/com/victone/life/LifeDriver.java
@@ -0,0 +1,108 @@
+package com.victone.life;
+
+import com.victone.life.logic.LifeBoard;
+
+import java.util.Scanner;
+
+public class LifeDriver {
+
+	private static LifeBoard gb;
+	private static boolean consoleMode = false;
+
+	public static void main(String[] args) {
+		if (args.length != 0)
+			consoleMode = true; // if args are present we assume no gui
+		if (consoleMode) { // no gui window
+			try { // get arts
+				gb = new LifeBoard(Integer.parseInt(args[0]),
+						Integer.parseInt(args[1]), consoleMode);
+			} catch (Exception e) {
+				gb = new LifeBoard(76, 18, consoleMode); 
+				// looks good with standard console dimensions
+			}
+			while (true) { // this input section is buggy and was really just
+							// for testing
+				displayMenu();
+				Scanner scanner = new Scanner(System.in);
+				String input = "";
+				while (input.length() == 0) {
+					input = scanner.nextLine();
+				}
+				switch (input.toUpperCase().charAt(0)) {
+				case 'C':
+					clearBoard();
+					break;
+				case 'R':
+					gb.randomize();
+					break;
+				case 'Q':
+					System.out.println("Thanks for Playing!");
+					System.exit(0);
+				case 'S':
+					gb.step();
+					break;
+				case 'X':
+					clickCellConsole();
+				}
+			}
+		} else { // swing'n'such
+			gb = new LifeBoard(LifeBoard.getDefaultBoardWidth(),
+					LifeBoard.getDefaultBoardHeight(), consoleMode);
+		}
+	}
+
+	private static void clearBoard() {
+		gb.clearBoard();
+	}
+
+	private static void clickCellConsole() { // no error correction
+		int x = -1, y = -1;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter X Coordinate (1 - " + gb.getWidth() + ") ");
+		if (scanner.hasNextInt())
+			x = scanner.nextInt() - 1;
+		
+		System.out.print("Enter Y Coordinate (1 - " + gb.getHeight() + ") ");
+
+		if (scanner.hasNextInt())
+			y = scanner.nextInt() - 1;
+
+		if (!gb.click(x, y)) {
+			System.out.println("Incorrect input.");
+			clickCellConsole();
+		}
+	}
+
+	private static void displayMenu() {
+		clearScreen();
+		System.out.println("Conway's 'Life', implemented by Victor Wilson");
+		System.out.print(gb);
+		System.out.println("Generation: " + gb.getGenerations());
+		System.out.print("(S) to Step the Board");
+		System.out.print("\t(X) to Click a Cell  ");
+		System.out.println("(R) to Randomize the Board");
+		System.out.print("(C) to Clear the Board  ");
+		System.out.println("(Q) to Quit");
+		System.out.print("Command: ");
+	}
+
+	private static void clearScreen() {
+		String osName = System.getProperty("os.name");
+		boolean isMac = osName.matches("(?i).*os x*");
+
+		//this doesn't work
+//		if (osName.toLowerCase().contains("windows")) {
+//			try { 
+//				Runtime.getRuntime().exec("cls");
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+
+		if (isMac) { //this is standard unix clear screen code
+			System.out.print("\033[H\033[2J");
+		}
+	}
+}
---
+++ src/main/java/com/victone/life/ui/LifeMainGUI.java
@@ -0,0 +1,280 @@
+package com.victone.life.ui;
+
+import com.victone.life.logic.LifeBoard;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//import java.awt.event.MouseEvent;
+//import java.awt.event.MouseMotionListener;
+
+public class LifeMainGUI extends JFrame implements ActionListener {
+
+	private static final String LABEL = "Jim Conway's The Game of Life (implemented by Victor Wilson)";
+
+	private static final String VERSION = "1.1";
+	private static final long serialVersionUID = 715255226314002424L;
+	private static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 680;
+
+	private static final String HELP_STRING = "Jim Conway's Game of Life\nWritten by Victor Wilson Sep 8-13 2012\n\n"
+			+ "Any live cell with 2 or 3 neighbors lives. Otherwise, it dies.\n"
+			+ "Any dead cell with exactly three neighbors springs forth.";
+
+	// private MouseEvent previousMouseEvent = null;
+
+	private LifeBoard gameBoard;
+
+	private JLabel generationsLabel, frequencyLabel;
+	private JButton stepButton, clearButton, autoButton, randomButton,
+			fastButton, slowButton, helpButton;
+	private JButton buttonBoard[][];
+	private JPanel gamePanel, controlPanel;
+
+	public LifeMainGUI(LifeBoard gb) {
+		super(LABEL + " version " + VERSION);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		setSize(FRAMEWIDTH, FRAMEHEIGHT);
+		//setResizable(false);
+
+		gameBoard = gb;
+
+		buildGUI();
+		setVisible(true);
+	}
+
+	public void buildGUI() {
+		gamePanel = new JPanel(new GridLayout(gameBoard.getHeight(),
+				gameBoard.getWidth()));
+		controlPanel = new JPanel(new FlowLayout());
+
+		generationsLabel = new JLabel("Generation: 0");
+		frequencyLabel = new JLabel("Frequency: " + gameBoard.getFrequency());
+
+		initButtonBoard();
+
+		populateGamePanel();
+
+		initControlButtons();
+
+		addControlListeners();
+
+		addGameBoardListeners();
+
+		populateControlPanel();
+
+		// this is last
+		add(gamePanel, BorderLayout.CENTER);
+		add(controlPanel, BorderLayout.SOUTH);
+	}
+
+	private void populateGamePanel() {
+		// add buttonBoard to the panel
+		for (int i = 0; i < gameBoard.getHeight(); i++) {
+			for (int j = 0; j < gameBoard.getWidth(); j++) {
+				gamePanel.add(buttonBoard[i][j]);
+			}
+		}
+	}
+
+	private void initControlButtons() {
+		// newButton = new JButton("New");
+		// newButton
+		// .setToolTipText("Create a new Game Board with different dimensions");
+		// newButton.setEnabled(false); // enable this eventually
+
+		helpButton = new JButton("Help");
+		helpButton.setToolTipText("Instructions & Information");
+
+		clearButton = new JButton("Clear");
+		clearButton
+				.setToolTipText("Clear the simulation and reset the generation count");
+
+		stepButton = new JButton("Step");
+		stepButton.setToolTipText("Advance the simulation by one step");
+
+		autoButton = new JButton("Auto");
+		autoButton.setToolTipText("Automatically advance the simulation");
+
+		randomButton = new JButton("Randomize");
+		randomButton.setToolTipText("Randomly populate the simulation and reset the generation count");
+
+		fastButton = new JButton("Faster");
+		fastButton.setToolTipText("Increase the speed of the simulation");
+		//fastButton.setEnabled(false);
+
+		slowButton = new JButton("Slower");
+		slowButton.setToolTipText("Decrease the speed of the simulation");
+		//slowButton.setEnabled(false);
+	}
+
+	private void addControlListeners() {
+		// newButton.addActionListener(this);
+		helpButton.	 addActionListener(this);
+		clearButton. addActionListener(this);
+		stepButton.  addActionListener(this);
+		autoButton.  addActionListener(this);
+		randomButton.addActionListener(this);
+		fastButton.  addActionListener(this);
+		slowButton.  addActionListener(this);
+	}
+
+	private void populateControlPanel() {
+		// controlPanel.add(newButton);
+		controlPanel.add(helpButton);
+		controlPanel.add(clearButton);
+		controlPanel.add(stepButton);
+		controlPanel.add(randomButton);
+		controlPanel.add(autoButton);
+		controlPanel.add(generationsLabel);
+		controlPanel.add(slowButton);
+		controlPanel.add(frequencyLabel);
+		controlPanel.add(fastButton);
+	}
+
+	private void addGameBoardListeners() {
+		for (int i = 0; i < gameBoard.getHeight(); i++) {
+			for (int j = 0; j < gameBoard.getWidth(); j++) {
+				buttonBoard[i][j].addActionListener(this);
+				//buttonBoard[i][j].addMouseMotionListener(this);
+			}
+		}
+	}
+	
+	private void initButtonBoard() {
+		int height = gameBoard.getHeight(), width = gameBoard.getWidth();
+		buttonBoard = new JButton[height][width];
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) //take button from cell
+				buttonBoard[i][j] = gameBoard.getCell(j, i).getButton();
+		}
+	}
+
+	public void updateCounter() {
+		generationsLabel.setText("Generation: " + gameBoard.getGenerations());
+	}
+	
+	public void updateFrequency() {
+		frequencyLabel.setText("Frequency: " + gameBoard.getFrequency());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String command = arg0.getActionCommand();
+
+		if (command.equals("New")) {
+			// implement me
+		} else if (command.equals("Help")) {
+			help();
+		} else if (command.equals("Clear")) {
+			clear();
+		} else if (command.equals("Step")) {
+			step();
+		} else if (command.equals("Randomize")) {
+			randomize();
+		} else if (command.equals("Auto") || command.equals("Stop")) {
+			auto();
+		} else if (command.equals("Faster")) {
+			faster();
+		} else if (command.equals("Slower")) {
+			slower();
+		} else {
+			// parse button name into x and y coordinates
+			String split[] = command.split("[.]");
+			gameBoard.click(Integer.parseInt(split[0]),
+					Integer.parseInt(split[1]));
+		}
+	}
+
+	private void help() {
+		JOptionPane.showMessageDialog(null, HELP_STRING, "Help",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	private void clear() {
+		if (gameBoard.getAuto()) {
+			auto();
+		}
+		gameBoard.clearBoard();
+		updateCounter();
+	}
+
+	private void step() {
+		gameBoard.step();
+		updateCounter();
+	}
+
+	private void randomize() {
+		gameBoard.randomize();
+		updateCounter();
+	}
+
+	private void auto() { // this required threading to work
+		if (!gameBoard.getAuto()) {
+			autoButton.setText("Stop");
+			autoButton
+			.setToolTipText("Stop automatic advancing of the simulation");
+			stepButton.setEnabled(false);
+			//fastButton.setEnabled(true);
+			//slowButton.setEnabled(true);
+			gameBoard.auto();
+		} else {
+			autoButton.setText("Auto");
+			autoButton.setToolTipText("Automatically advance the simulation");
+			stepButton.setEnabled(true);
+			//fastButton.setEnabled(false);
+			//slowButton.setEnabled(false);
+			gameBoard.auto();
+		}
+	}
+
+	private void faster() {
+		gameBoard.incrementFrequency();
+		updateFrequency();
+	}
+
+	private void slower() {
+		gameBoard.decrementFrequency();
+		updateFrequency();
+	}
+
+//		@SuppressWarnings("unused")
+//	private void click(JButton jb) { // not used so far
+//		String split[] = jb.getActionCommand().split("[.]");
+//		gameBoard.click(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+//	}
+
+//	@Override
+//	public void mouseMoved(MouseEvent arg0) { // not used
+//		if (arg0.getComponent() instanceof JButton) {
+//
+//		}
+//	}
+
+//	@Override
+//	public void mouseDragged(MouseEvent arg0) { // doesn't work
+//		/*
+//		 * // if (arg0.getComponent() instanceof JButton) { // if
+//		 * (previousMouseEvent == null) { //initial case // previousMouseEvent =
+//		 * arg0; // JButton jb = (JButton) previousMouseEvent.getComponent(); //
+//		 * String[] s = jb.getActionCommand().split("[.]"); //
+//		 * gameBoard.click(Integer.parseInt(s[0]), Integer.parseInt(s[1])); // }
+//		 * else { // subsequent cases // JButton jb = (JButton)
+//		 * previousMouseEvent.getComponent(); // JButton argjb = (JButton)
+//		 * arg0.getComponent(); // if(
+//		 * !jb.getActionCommand().equals(argjb.getActionCommand())) { //
+//		 * String[] s = jb.getActionCommand().split("[.]"); //
+//		 * gameBoard.click(Integer.parseInt(s[0]), Integer.parseInt(s[1])); // }
+//		 * else { // // } // // // } // previousMouseEvent = arg0; // JButton jb
+//		 * = (JButton) arg0.getComponent(); // String[] s =
+//		 * jb.getActionCommand().split("[.]"); //
+//		 * gameBoard.click(Integer.parseInt(s[0]), Integer.parseInt(s[1])); // }
+//		 * 
+//		 * // String origin = (JButton) arg0.getComponent().getActionCommand();
+//		 * // System.out.println("bloop"); // String command = (JButton)
+//		 * arg0.getComponent() // if
+//		 */
+//	}
+
+}
